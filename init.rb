0d528049143ad1665adedbdc42bb9b177a4697c7
Kenji Okimoto <okimoto@clear-code.com>
2018-11-02 14:51:47 +0900

    Support commonmarker 0.18.1
    Fix #4

--- init.rb
+++ init.rb
@@ -9,6 +9,24 @@ Redmine::Plugin.register :common_mark do
   description "This plugin provides CommonMark notation"
   url "https://github.com/okkez/redmine_common_mark"
   author_url "https://github.com/okkez/redmine_common_mark"
+  settings default: {
+             parse_validate_utf8: "0",
+             parse_smart: "0",
+             parse_liberal_html_tag: "0",
+             parse_footnotes: "0",
+             parse_strikethrough_double_tilde: "0",
+             render_sourcepos: "0",
+             render_hardbreaks: "1",
+             render_unsafe: "0",
+             render_nobreaks: "0",
+             render_github_pre_lang: "0",
+             render_table_prefer_style_attributes: "0",
+             render_full_info_string: "0",
+             extension_table: "1",
+             extension_strikethrough: "1",
+             extension_autolink: "1",
+             extension_tagfilter: "1"
+           }, partial: "settings/common_mark"
 end
 
 Redmine::WikiFormatting.map do |format|
