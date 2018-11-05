1212a1724f589a4b69b09e8a0242e79fece25e44
Kenji Okimoto <okimoto@clear-code.com>
2018-11-05 10:53:27 +0900

    Release v0.3.1

--- init.rb
+++ init.rb
@@ -4,7 +4,7 @@ require "redmine/wiki_formatting/common_mark/html_parser"
 
 Redmine::Plugin.register :common_mark do
   name "CommonMark plugin"
-  version "0.3.0"
+  version "0.3.1"
   author "Kenji Okimoto"
   description "This plugin provides CommonMark notation"
   url "https://github.com/okkez/redmine_common_mark"
